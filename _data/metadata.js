ce986eda409a053b24b93cce0622f809209d3dca
mt40 <minhthai40@gmail.com>
2025-04-02 16:31:09 +0700

    add header

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,14 @@
 export default {
-	title: "Eleventy Base Blog v9",
+	title: "Specs From Above",
 	url: "https://example.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
+		name: "Preston Pham",
+		email: "preston.pham@fastmail.com",
+		linkedin: "https://www.linkedin.com/in/prestonp-swe",
+		blog: "https://mangotreeblog.substack.com"
+	},
+	github: "https://github.com/mt40/spec-from-above",
+	contribute_url: "https://github.com/mt40/spec-from-above"
 }
